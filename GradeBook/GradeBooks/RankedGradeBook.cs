8755c38f387578ec741c6be73fb6c2d2149be687
Hiten1928 <hg1928@rit.edu>
2018-05-01 18:48:01 -0400

    Module 2, Override ranked class method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,9 +7,22 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+        
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("We need at least 5 students to do ranking.");
+            }
+            else {
+                return base.GetLetterGrade(averageGrade);
+            }
+        }
+
     }
 }
