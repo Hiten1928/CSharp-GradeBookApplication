1e07b4b33772e0c441dade233e2c9cac837cd182
Hiten1928 <hg1928@rit.edu>
2018-05-02 16:49:55 -0400

    Ranked gradebook methods added

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -16,12 +17,22 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            {
+            
                 throw new InvalidOperationException("We need at least 5 students to do ranking.");
-            }
-            else {
-                return base.GetLetterGrade(averageGrade);
-            }
+            
+            var threshold =  (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if ([(threshold * 3) - 1])
+                return 'C';
+            else if ([(threshold * 4) - 1])
+                return 'D';
+            else
+                return 'F';
         }
 
     }
